fc3c578271d93f27530f3e4aadec341e6533b75e
nugo <nugo.ezenwa@outlook.com>
2025-10-04 22:02:46 -0400

    updated the key pair

--- modules/github-self-hosted-runner/variables.tf
+++ modules/github-self-hosted-runner/variables.tf
@@ -13,7 +13,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "The key name for the Jenkins server"
   type        = string
-  default     = "DevOps_Train2024"
+  default     = "Key40"
 }
 
 variable "main_region" {
