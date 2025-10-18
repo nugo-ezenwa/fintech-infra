9fb57061f3097e46c39784dc9adbfa9c5757d83e
nugo <nugo.ezenwa@outlook.com>
2025-10-17 23:48:50 -0400

    ran formatting

--- prod/main.tf
+++ prod/main.tf
@@ -6,9 +6,9 @@
 ###-start here
 terraform {
   required_providers {
-    aws        = { source = "hashicorp/aws",        version = "~> 5.0" }
+    aws        = { source = "hashicorp/aws", version = "~> 5.0" }
     kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.30" }
-    helm       = { source = "hashicorp/helm",       version = "~> 2.13" }
+    helm       = { source = "hashicorp/helm", version = "~> 2.13" }
   }
 }
 
