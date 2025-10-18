ba355d02cd6190fd98decd062939a3f3bd854498
nugo <nugo.ezenwa@outlook.com>
2025-10-17 23:46:29 -0400

    added provider stuff

--- prod/main.tf
+++ prod/main.tf
@@ -2,6 +2,39 @@
 # # VPC Module
 # ################################################################################
 
+
+###-start here
+terraform {
+  required_providers {
+    aws        = { source = "hashicorp/aws",        version = "~> 5.0" }
+    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.30" }
+    helm       = { source = "hashicorp/helm",       version = "~> 2.13" }
+  }
+}
+
+provider "aws" {
+  region = var.main_region
+}
+
+# Live cluster connection details (use the var, not module output, to avoid loops)
+data "aws_eks_cluster" "this" {
+  name = var.cluster_name
+}
+data "aws_eks_cluster_auth" "this" {
+  name = var.cluster_name
+}
+
+# Provider that talks to EKS (NOT localhost)
+provider "kubernetes" {
+  alias                  = "eks"
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+  load_config_file       = false
+}
+
+#######-stop here
+
 module "vpc" {
   source      = "./../modules/vpc"
   main_region = var.main_region
